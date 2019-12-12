15c8d00e2c3c31941fd091a0dc088069bb5eca5b
Bartek Nowakowski <bartek@bnowakowski.pl>
2019-12-12 07:54:45 +0100

    starting crashplan engine, cat password to console

--- startup.sh
+++ startup.sh
@@ -44,7 +44,8 @@ fi
 
 cd /home/ubuntu
 apt update
-apt install -y wget cpio openjdk-8-jre-headless libxss1
+apt install -y wget cpio libxss1
+# openjdk-8-jre-headless
 #wget https://download.code42.com/installs/agent/7.2.0/1641/install/CrashPlanSmb_7.2.0_1525200006720_1641_Linux.tgz
 wget https://download.code42.com/installs/agent/7.4.0/566/install/CrashPlanSmb_7.4.0_1525200006740_566_Linux.tgz
 echo -e "dupa\ndupa" | passwd
@@ -53,7 +54,9 @@ echo 'JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64/bin/"' >> /etc/environment
 tar zxf CrashPlanSmb_7.4.0_1525200006740_566_Linux.tgz
 cd crashplan-install
 echo -e "\n\n\n\n\n\n\n\n\n\n\n\n" | ./install.sh
+/usr/local/crashplan/bin/CrashPlanEngine start
 cd /home/ubuntu
+cat password.txt 
 
 /usr/bin/supervisord -n
 
